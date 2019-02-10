aac89fce3a319352e1a44e91b70cfafd5abc7bf0
Francisco Javier García Gutiérrez <javiergarcia.tech@gmail.com>
2019-02-10 13:14:17 -0600

    Shader - Analog Glitch Update

---
+++ Shader/Camera/Analog Glitch/AnalogGlitch.cs
@@ -0,0 +1,217 @@
+﻿/* 
+ * Based on the AnalogGlitch shader on KinoGlitch repo from Keijiro.
+ * https://github.com/keijiro/KinoGlitch
+*/
+
+using UnityEngine;
+
+namespace Framework.CameraShader {
+
+    /// <summary>
+    /// Analog Glitch
+    /// By Javier García, 2019.
+    /// 
+    /// <para>
+    /// Controller for analog glitch camera shader.
+    /// </para>
+    /// </summary>
+    [ExecuteInEditMode]
+    [RequireComponent (typeof (Camera))]
+    public class AnalogGlitch: MonoBehaviour {
+
+
+        #region Class Members
+
+        [SerializeField]
+        Shader _shader;                     //  Shader.
+        Material _material;                 //  Material.
+
+        [Header ("Grain Noise")]
+        [SerializeField, Range (-1f, 1f)]
+        private float _strength;            //  Strength of noise.
+        [SerializeField]
+        private float _scale;               //  Scale of noise.
+        [SerializeField]
+        private Vector3 _parameters;        //  Parameters for noise.
+
+        [Header ("Positional Glitch")]
+        [SerializeField]
+        private float _verticalJump;        //  Horizontal jump.
+        [SerializeField]
+        private float _horizontalShake;     //  Amount of shake.
+        [SerializeField]
+        private float _colorDrift;          //  Amount of drift.
+        private float _jumpTime;
+
+        [Header ("Jitter")]
+        [SerializeField, Range (0f, 1f)]
+        private float _displacement;        //  Jitter Displacement.
+        [SerializeField, Range (0f, 1f)]
+        private float _threshold;           //  Jitter Threshold.
+        [SerializeField]
+        private JitterFunctions _function;  //  Type of function.
+        [SerializeField]
+        private float _amplitude;           //  Amplitud of functions.
+        [SerializeField]
+        private float _frequency;           //  Frequency of functions.
+        [SerializeField]
+        private Vector2 _offset;            //  Offset of functions.
+
+        #endregion
+
+
+        #region Accesors
+
+        /// <summary> Gets the shader. </summary>
+        private Shader Shader {
+            get {
+                if (_shader == null)
+                    _shader = Shader.Find ("Hidden/JGFramework/Camera/AnalogGlitch");
+                return _shader;
+            }
+        }
+
+        /// <summary> Gets or sets the strength. </summary>
+        public float Strength {
+            get { return _strength; }
+            set { _strength = value; }
+        }
+
+        /// <summary> Gets or sets the noise parameters. </summary>
+        public Vector3 NoiseParameters {
+            get { return _parameters; }
+            set { _parameters = value; }
+        }
+
+        /// <summary> Gets or sets the scale. </summary>
+        public float Scale {
+            get { return _scale; }
+            set { _scale = value; }
+        }
+
+        /// <summary> Gets or sets the vertical jump. </summary>
+        public float VerticalJump {
+            get { return _verticalJump; }
+            set { _verticalJump = value; }
+        }
+
+        /// <summary> Gets or sets the horizontal scale. </summary>
+        public float HorizontalScale {
+            get { return _horizontalShake; }
+            set { _horizontalShake = value; }
+        }
+
+        /// <summary> Gets or sets the color drift. </summary>
+        public float ColorDrift {
+            get { return _colorDrift; }
+            set { _colorDrift = value; }
+        }
+
+        /// <summary> Gets or sets the displacement. </summary>
+        public float Displacement {
+            get { return _displacement; }
+            set { _displacement = value; }
+        }
+
+        /// <summary> Gets or sets the threshold. </summary>
+        public float Threshold {
+            get { return _threshold; }
+            set { _threshold = value; }
+        }
+
+        /// <summary> Gets or sets the function. </summary>
+        public JitterFunctions Function {
+            get { return _function; }
+            set { _function = value; }
+        }
+
+        /// <summary> Gets or sets the amplitud. </summary>
+        public float Amplitud {
+            get { return _amplitude; }
+            set { _amplitude = value; }
+        }
+
+        /// <summary> Gets or sets the frequency. </summary>
+        public float Frequency {
+            get { return _frequency; }
+            set { _frequency = value; }
+        }
+
+        /// <summary> Gets or sets the offset. </summary>
+        public Vector2 Offset {
+            get { return _offset; }
+            set { _offset = value; }
+        }
+
+        #endregion
+
+
+        #region MonoBehaviour Overrides
+
+        //   Called on render image.
+        void OnRenderImage (RenderTexture source, RenderTexture destination) {
+
+            if (_material == null)
+                _material = new Material (Shader) {
+                    hideFlags = HideFlags.DontSave
+                };
+
+
+            //  Changing the grain properties.
+            _material.SetFloat ("_Strength", _strength);
+            _material.SetFloat ("_Scale", _scale);
+            _material.SetVector ("_NoiseParameters", _parameters);
+
+            //  Changing the vertical jump properties.
+            _jumpTime += Time.deltaTime * _verticalJump;
+            _material.SetFloat ("_Jump", _verticalJump);
+            _material.SetFloat ("_JumpTime", _jumpTime);
+
+            //  Changing the horizontal shake properties.
+            _material.SetFloat ("_Shake", _horizontalShake);
+
+            //  Changing the color drifter properties.
+            _material.SetFloat ("_Drift", _colorDrift);
+            _material.SetFloat ("_DriftTime", Time.time);
+
+            //  Changing the Jitter properties.
+            _material.SetFloat ("_Displacement", _displacement);
+            _material.SetFloat ("_Threshold", _threshold);
+
+            switch (_function) {
+                case JitterFunctions.SINC:
+                _material.SetFloat ("_Sinc", 1f);
+                _material.SetFloat ("_Sigmoid", 0f);
+                break;
+                case JitterFunctions.SIGMOID:
+                _material.SetFloat ("_Sigmoid", 1f);
+                _material.SetFloat ("_Sinc", 0f);
+                break;
+                default:
+                _material.SetFloat ("_Sinc", 0f);
+                _material.SetFloat ("_Sigmoid", 0f);
+                break;
+            }
+
+            _material.SetFloat ("_Amplitude", _amplitude);
+            _material.SetFloat ("_Frequency", _frequency);
+            _material.SetVector ("_Offset", _offset);
+
+            Graphics.Blit (source, destination, _material);
+        }
+
+        #endregion
+
+
+        #region Sub Classes
+
+        /// <summary>  functions. </summary>
+        public enum JitterFunctions {
+            NONE,
+            SINC,
+            SIGMOID
+        }
+
+        #endregion
+    }
+}
---
+++ Shader/Camera/Analog Glitch/AnalogGlitch.shader
@@ -0,0 +1,202 @@
+﻿//  Based on the following projects:
+//  The Grain Baker Shader on PostProcessing repo from Unity-Technologies.
+//  https://github.com/Unity-Technologies/PostProcessing
+//  The WhiteNoise gist of josephbk117.
+//  https://gist.github.com/josephbk117/884900c161705738d1b2fc4be38c5910
+//  The AnalogGlitch on KinoGlitch repo from Keijiro.
+//  https://github.com/keijiro/KinoGlitch
+
+
+Shader "Hidden/JGFramework/Camera/AnalogGlitch"{
+    Properties{
+        _MainTex ("Albedo", 2D) = "white"  {}
+        
+        [Header(Grain Noise)]
+        _Strength("Strength", Range(-1,1)) = 0
+        _Scale("Scale", float) = 100
+        _Phase("Phase", float) = 0
+        _NoiseParameters("Noise", Vector) = (0,0,0,0)
+        
+        [Header(Positional Glitch)]
+        _Jump("Vertical Jump", Float) = 0
+        _Shake("Horizontal Shake", Float) = 0
+        _Drift("Color Drift", Float) = 0
+        
+        [Header(Jitter)]
+        _Displacement ("Displacement", Range(0,1)) = 0
+        _Threshold ("Threshold", Range(0,1)) = 0
+        
+        [Header(Jitter Functions)]
+        _Sinc("Sinc Function", Range(0,1)) = 0
+        _Sigmoid("Sigmoid Function", Range(0,1)) = 0
+        
+        _Amplitude("Amplitude", Float) = 0
+        _Frequency("Frequency", Float) = 0
+        _Offset("Offset", Vector) = (0,0,0,0)
+    }
+    
+    
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    float4 _MainTex_ST;
+    
+    float _Strength;            //  Strength of noise.
+    float _Scale;               //  Scale of noise.
+    float4 _NoiseParameters;    //  Parameters for noise.
+    
+    float _Jump;                //  Horizontal jump.
+    float _JumpTime;            //  Jump timer.
+    float _Shake;               //  Amount of shake.
+    float _Drift;               //  Amount of drift.
+    float _DriftTime;           //  Drift timer.
+    
+    float _Displacement;        //  Jitter Displacement.
+    float _Threshold;           //  Jitter Threshold.
+    
+    float _Sinc;                //  Sinc enable value.
+    float _Sigmoid;             //  Sigmoid enable value.
+    float _Amplitude;           //  Amplitud of functions.
+    float _Frequency;           //  Frequency of functions.
+    float4 _Offset;             //  Offset of functions.
+    
+    struct appdata {            
+        float4 vertex : POSITION;
+        float3 normal : NORMAL;
+        float2 uv : TEXCOORD0;
+    };
+    
+    struct v2f {
+        float4 pos : SV_POSITION;
+        half2 uv   : TEXCOORD0; 
+        
+        float3 normal : NORMAL;
+        float3 noise_uv : TEXCOORD1;
+    };
+    
+    
+    //  Canonical 2D random function.
+    float random(float x, float y) {
+        float2 p = float2(x, y + x);
+        return frac(sin(dot(p.xy,float2(12.9898, 78.233))) * 43758.5453);
+    }
+    
+    //  Variation of canonical function.
+    float Noise(float2 n, float x) {
+        n += x;
+        return frac(sin(dot(n.xy, _NoiseParameters.xy)) * _NoiseParameters.z);
+    }
+    
+    //  Step one for noise.
+    float Step1(float2 uv, float n) {
+        float b = 2.0, c = -12.0;
+        return (1.0 / (4.0 + b * 4.0 + abs(c))) * (
+            Noise(uv + float2(-1.0, -1.0), n) +
+            Noise(uv + float2( 0.0, -1.0), n) * b +
+            Noise(uv + float2( 1.0, -1.0), n) +
+            Noise(uv + float2(-1.0,  0.0), n) * b +
+            Noise(uv + float2( 0.0,  0.0), n) * c +
+            Noise(uv + float2( 1.0,  0.0), n) * b +
+            Noise(uv + float2(-1.0,  1.0), n) +
+            Noise(uv + float2( 0.0,  1.0), n) * b +
+            Noise(uv + float2( 1.0,  1.0), n)
+        );
+    }
+    
+    //  Step two for noise.
+    float Step2(float2 uv, float n) {
+        float b = 2.0, c = 4.0;
+        return (1.0 / (4.0 + b * 4.0 + abs(c))) * (
+            Step1(uv + float2(-1.0, -1.0), n) +
+            Step1(uv + float2( 0.0, -1.0), n) * b +
+            Step1(uv + float2( 1.0, -1.0), n) +
+            Step1(uv + float2(-1.0,  0.0), n) * b +
+            Step1(uv + float2( 0.0,  0.0), n) * c +
+            Step1(uv + float2( 1.0,  0.0), n) * b +
+            Step1(uv + float2(-1.0,  1.0), n) +
+            Step1(uv + float2( 0.0,  1.0), n) * b +
+            Step1(uv + float2( 1.0,  1.0), n)
+        );
+    }
+    
+    //  Step three for noise.
+    float3 Step3(float2 uv) {
+        float a = Step2(uv, 0.07 * frac(0));
+        float b = Step2(uv, 0.11 * frac(0));
+        float c = Step2(uv, 0.13 * frac(0));
+        return float3(a, b, c);
+    }
+    
+    //  Cardinal sine function.
+    float sinc(float v) {
+        float pi = 3.1416;
+        float denominator = _Frequency * pi * v + _Offset.y;
+        float numerator = sin(denominator);
+        return _Offset.x + _Amplitude * numerator/denominator;
+    }
+    
+    // Logistic function sigmoid curve.
+    float sigmoid(float v) {
+        return _Offset.x + _Amplitude/(1 + exp(-_Frequency * (v - _Offset.y)));
+    }
+    
+    //  Pragma Vertex
+    v2f vert (appdata v) {
+        v2f o;
+        
+        o.normal = v.normal;
+        o.pos = UnityObjectToClipPos(v.vertex);
+        o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+        o.noise_uv = v.vertex.xyz/v.vertex.w;
+
+        return o;
+    }
+    
+    //  Pragma Fragment
+    half4 frag(v2f i) : SV_Target {
+        float u = i.uv.x;
+        float v = i.uv.y;
+        float2 uv = i.uv;
+        
+        //  Jitter
+        float jitter = random(v, _Time.x) * 2 - 1;
+        jitter *= step(_Threshold, abs(jitter)) * _Displacement;
+        jitter = _Sinc * sinc(v) * jitter + (1 - _Sinc) * jitter;
+        jitter = _Sigmoid * sigmoid(v) * jitter + (1 - _Sigmoid) * jitter;
+        
+        //  Vertical Jump
+        float jump = lerp(v, frac(v + _JumpTime), _Jump.x);
+        
+        //  Horizontal Shake
+        float shake = (random(_Time.x, 2) - 0.5) * _Shake;
+        
+        //  Color Drift
+        float drift = sin(jump + _DriftTime) * _Drift;
+        
+        //  Sources
+        float src = u + jitter + shake;
+        half4 src1 = tex2D(_MainTex, frac(float2(src, jump)));
+        half4 src2 = tex2D(_MainTex, frac(float2(src + drift, jump)));
+        
+        //  Grain
+        float2 uvScaled = floor(uv * _Scale);
+        float rand = random(uvScaled.x * _Time.x, uvScaled.y * _Time.x);
+        fixed3 grain = Step3(uv.xy * _Time.xy * rand);
+        
+        return half4(src1.r, src2.g, src1.b, 1) + float4(grain, 1) * _Strength;
+    }
+
+    ENDCG
+    SubShader {
+        Pass {
+            ZTest Always Cull Off ZWrite Off
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 3.0
+            ENDCG
+        }
+    }
+}
