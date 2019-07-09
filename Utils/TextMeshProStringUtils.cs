005f419600e3ca942c1aeaf9a6553477a87e38ca
Francisco Javier García Gutiérrez <javier.game.dev@gmail.com>
2019-07-09 14:02:46 -0500

    Utils - Text Mesh Pro

---
+++ Utils/TextMeshProStringUtils.cs
@@ -0,0 +1,17 @@
+﻿#if BRICKSBUCKET_TMPRO
+using TMPro;
+
+namespace BricksBucket.Utils
+{
+    public static class TextMeshProUtils
+    {
+        /// <summary> Updates TMP_Text Component Text. </summary>
+        /// <param name="label"></param>
+        /// <param name="text"></param>
+        public static void UpdateText (this TMP_Text label, params string[] text)
+        {
+            label.text = StringUtils.Concat (text);
+        }
+    }
+}
+#endif
