2a8f3b8cc7f8ecf30940e3cc975f7e81564e6608
Francisco Javier García Gutiérrez <javier@monogum.games>
2020-07-05 15:50:56 -0500

    Pooling - Prefab Types

---
+++ Pooling/PrefabType.cs
@@ -0,0 +1,21 @@
+﻿namespace BricksBucket.Core.Pooling
+{
+    /// <summary>
+    ///
+    /// PrefabType Enum.
+    ///
+    /// <para>
+    /// Defines the different types of prefabs for pooling.
+    /// </para>
+    ///
+    /// <para> By Javier García | @jvrgms | 2019 </para>
+    /// 
+    /// </summary>
+    public enum PrefabType
+    {
+        NONE,   //  No belongs to any type.
+        _3D,    //  Type for 3D environment.
+        _2D,    //  Type for 2D environment.
+        UI      //  Type to use in UI.
+    }
+}
