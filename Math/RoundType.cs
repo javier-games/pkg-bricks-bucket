830c0e6a5889e7b8cd5be7faf879612108f2078c
Francisco Javier García Gutiérrez <javier@monogum.games>
2020-07-05 13:54:00 -0500

    Math - Round Types

---
+++ Math/RoundType.cs
@@ -0,0 +1,30 @@
+﻿namespace BricksBucket.Core.Math 
+{
+    /// <!-- RoundType -->
+    /// 
+    /// <summary>
+    /// Defines the different approximation types.
+    /// </summary>
+    ///
+    /// <seealso cref="FloatExtensions"/>
+    /// <seealso cref="DoubleExtensions"/>
+    /// 
+    /// <!-- By Javier García | @jvrgms | 2020 -->
+    public enum RoundType
+    {
+        /// <summary>
+        /// Rounded to the nearest integer.
+        /// </summary>
+        ROUND = 0,
+        
+        /// <summary>
+        /// Smallest integer greater or equal.
+        /// </summary>
+        CEIL = 1,
+        
+        /// <summary>
+        /// Grater integer smallest or equal.
+        /// </summary>
+        FLOOR = 2
+    }
+}
