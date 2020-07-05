18c54ded149a01844749f2319b85213987ab776e
Francisco Javier García Gutiérrez <javier@monogum.games>
2020-07-05 13:45:07 -0500

    Editor/Classes - MonoBehaviour Editor

---
+++ Editor/MonoBehaviourEditor.cs
@@ -0,0 +1,67 @@
+using BricksBucket.Core.Attributes;
+using BricksBucket.Core.Attributes.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace BricksBucket.Core.Editor
+{
+	// ReSharper disable CommentTypo
+	/// <!-- MonoBehaviourEditor -->
+	/// 
+	/// <summary>
+	///
+	/// <para>
+	/// Override Editor for a general MonoBehaviour. Adds the functionality to
+	/// display methods as buttons that use the <see
+	/// cref="ButtonAttribute"/> on inspector.
+	/// </para>
+	///
+	/// <para>
+	/// This class is canceled by OdinInspector, do not use both together.
+	/// </para>
+	/// 
+	/// </summary>
+	///
+	/// <seealso cref="ButtonAttribute"/>
+	///
+	/// <!-- By Javier García | @jvrgms | 2020 -->
+	// ReSharper restore CommentTypo
+	[CustomEditor (typeof (MonoBehaviour), true), CanEditMultipleObjects]
+	public class MonoBehaviourEditor : UnityEditor.Editor
+	{
+		#region Properties
+
+		/// <summary>
+		/// Current target.
+		/// </summary>
+		private MonoBehaviour _target;
+
+		/// <summary>
+		/// Handler for methods.
+		/// </summary>
+		private ButtonAttributeHandler _handler;
+
+		#endregion
+
+
+		#region Methods
+
+		/// <summary> Called on enable. </summary>
+		private void OnEnable ()
+		{
+			_target = target as MonoBehaviour;
+			if (_target == null) return;
+
+			_handler = new ButtonAttributeHandler (_target);
+		}
+
+		/// <inheritdoc cref="UnityEditor.Editor.OnInspectorGUI"/>
+		public override void OnInspectorGUI ()
+		{
+			base.OnInspectorGUI ();
+			_handler?.OnInspectorGUI ();
+		}
+
+		#endregion
+	}
+}
