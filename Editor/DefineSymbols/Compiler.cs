0f14f80fa2097c6b504336365a2dd1d4f00bfe9d
Francisco Javier García Gutiérrez <javier@monogum.games>
2020-07-05 15:45:48 -0500

    Editor/DefineSymbols - Compiler Types

---
+++ Editor/DefineSymbols/Compiler.cs
@@ -0,0 +1,22 @@
+﻿namespace BricksBucket.Core.Editor.DefineSymbols 
+{
+    /// <!-- Compiler -->
+    ///
+    /// <summary>
+    /// <para>
+    /// Defines the different compiler types to Defined Symbols.
+    /// </para>
+    /// <para>
+    /// Based in the UnityDefineManager by @karl-.
+    /// https://github.com/karl-/UnityDefineManager
+    /// </para>
+    /// </summary>
+    /// 
+    /// <!-- By Javier García | @jvrgms | 2020 -->
+    public enum Compiler
+    {
+        C_SHARP = 0,     //  Global define symbols for C# scripts.
+        EDITOR = 1,     //  Global define symbols for Editor scripts.
+        PLATFORM = 2    //  Global define symbols per platform.
+    }
+}
