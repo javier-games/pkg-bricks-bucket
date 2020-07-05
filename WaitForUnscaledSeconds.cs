575a9b4420e4155523d8a3ce7ea528945d11a4e2
Francisco Javier García Gutiérrez <javier@monogum.games>
2020-07-05 13:38:58 -0500

    Basic Classes - WaitForUnscaledSeconds

---
+++ WaitForUnscaledSeconds.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace BricksBucket.Core
+{
+	/// <!-- WaitForUnscaledSeconds -->
+	///
+	/// <summary>
+	/// Suspends the coroutine execution for the given amount of seconds
+	/// without using scaled time.
+	/// </summary>
+	/// 
+	/// <!-- By Javier García | @jvrgms | 2020 -->
+	public class WaitForUnscaledSeconds : CustomYieldInstruction
+	{
+		#region Class Members
+
+		/// <summary> Time to wait. </summary>
+		private readonly float _waitTime;
+
+		#endregion
+
+		
+		#region Accessors
+
+		/// <summary> Whether keep waiting or stops. </summary>
+		public override bool keepWaiting =>
+			Time.realtimeSinceStartup < _waitTime;
+
+		#endregion
+
+		
+		#region Constructor
+
+		/// <summary>
+		/// Suspends the coroutine execution for the given amount of
+		/// seconds without using scaled time.
+		/// </summary>
+		/// <param name="waitTime"> Time in seconds to wait. </param>
+		public WaitForUnscaledSeconds (float waitTime) =>
+			_waitTime = Time.realtimeSinceStartup + waitTime;
+
+		#endregion
+	}
+}
