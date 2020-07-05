be7c129118175055f92a8769c7155eccbc0c10f5
Francisco Javier García Gutiérrez <javier@monogum.games>
2020-07-05 15:46:04 -0500

    Editor/DefineSymbols - Define Symbol Class

---
+++ Editor/DefineSymbols/DefineSymbols.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BricksBucket.Core.Editor.DefineSymbols
+{
+    /// <!-- DefineSymbols -->
+    ///
+    /// <summary>
+    /// <para>
+    /// Stores the information of defined symbols.
+    /// </para>
+    /// <para>
+    /// Based in the UnityDefineManager by @karl-.
+    /// https://github.com/karl-/UnityDefineManager
+    /// </para>
+    /// </summary>
+    /// 
+    /// <!-- By Javier García | @jvrgms | 2020 -->
+    public class DefineSymbols : ScriptableObject
+    {
+        #region Class Members
+
+        [SerializeField]
+        protected Compiler m_compiler;               //  Compiler.
+
+        [SerializeField]
+        protected BuildTargetGroup m_buildTarget;    //  Platform.
+
+        [SerializeField]
+        protected string[] m_defines;                //  Symbols.
+
+        [SerializeField]
+        protected bool m_isApplied;                  //  Flag to apply.
+
+        #endregion
+    }
+}
